42541409bd72c997b685894124e75fdea89fd9c3
João Justo <jpjustonunes@hotmail.com>
2015-10-13 15:30:41 +0100

    Navbar menu

---
+++ app/scripts/menu.js
@@ -0,0 +1,17 @@
+$(function() {
+  const closeClass = 'Nav-overvlayHide';
+
+  let $close = $('.Nav-close');
+  let $burger = $('.Nav-burger');
+  let $overlay = $('.Nav-overlay');
+  let $navLinks = $('.Nav-overlayLink');
+
+  function toggleMenu() {
+    console.log('here')
+    $overlay.toggleClass(closeClass);
+  }
+
+  $close.click(toggleMenu);
+  $burger.click(toggleMenu);
+  $navLinks.click(toggleMenu);
+});
