0df83b590318234ec518ba14882b25c34057f820
João Justo <jpjustonunes@hotmail.com>
2015-10-16 15:00:53 +0100

    Prevents scroll when menu is open

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -1,13 +1,17 @@
 $(function(){
   const closeClass = 'is-hidden';
+  const noScrollClass = 'u-noScroll';
 
+  var body = $('body');
   var close = $('.Overlay-close');
   var burger = $('.Nav-burger');
   var overlay = $('.Overlay');
   var navLinks = $('.Overlay-link');
 
   function toggleMenu() {
+    body.toggleClass(noScrollClass);
     overlay.toggleClass(closeClass);
+
   }
 
   close.click(toggleMenu);
