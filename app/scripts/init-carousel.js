afa0456fe27903d366ab7ff855a04178355d6ceb
sight <sight.pedro@gmail.com>
2016-08-29 15:05:52 +0100

    init carousel, added section panel and stylings add testimonials carousel * using slider-pro plugin obtained from bower * config file for carousel sits at init-carousel.js

---
+++ app/scripts/init-carousel.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    $('#slider').sliderPro({
+        arrows: true,
+        width: 1000,
+        autoplay: false,
+        fadeArrows: false,
+        autoHeight: true,
+        init: function() {  // Run sliderResize function on slider init
+            sliderResizeToTallest();
+        },
+        sliderResize: function(){   // Run on sliderResize, this doesn't happen often enough that it would cause performance issues
+            sliderResizeToTallest();
+        }
+    });
+
+    // resize slider to tallest slide
+    function sliderResizeToTallest() {
+        var max = 0;
+
+        $('.sp-slide').each(function(el){
+            var slideHeight = $(this).height();
+            if( slideHeight > max )
+                max = slideHeight;
+        });
+
+        $('#slider').css({'height' : max});
+    }
+
+});
